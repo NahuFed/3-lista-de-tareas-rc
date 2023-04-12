bda38c264b83a7f5b8cd27d3fa54bad0b84a6e66
Nahuel Rodriguez <nahufed@gmail.com>
2023-04-12 20:22:47 -0300

    corregido script

--- js/app.js
+++ js/app.js
@@ -1,49 +1,43 @@
-let formulario = document.querySelector('form')
-let lista = document.querySelector('ol')
-let identificador = 0
+let formulario = document.querySelector("form");
+let lista = document.querySelector("ol");
+let identificador = 0;
 
-class Tarea{
-    constructor(nombre,id){
-        this.nombre = nombre
-        this.id = id
-    }
+class Tarea {
+  constructor(nombre, id) {
+    this.nombre = nombre;
+    this.id = id;
+  }
 }
 
-let tareas = []
+let tareas = [];
 
-function crearTarea(nombre){
-    tareas.push( new Tarea(nombre, identificador))    
+function crearTarea(nombre) {
+  tareas.push(new Tarea(nombre, identificador));
+}
+function capturarDatos(e) {
+  e.preventDefault();
+  agregarTarea(formulario.tarea.value);
+}
+function agregarTarea(tarea) {
+  crearTarea(tarea);
+  let elementoLista = document.createElement("li");
+  elementoLista.innerHTML = `${tarea}<button class="ms-3 btn  btn-borrar" id="${identificador}"><i class="bi bi-x-square-fill"></i></button>`;
+  lista.appendChild(elementoLista);
+  let botonesEliminar = document.querySelectorAll(".btn-borrar");
+  botonesEliminar.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      borrarTarea(boton.id);
+    });
+  });
+  identificador++;
 }
 
-
-
-
-
-function capturarDatos(e){
-    e.preventDefault()
-    agregarTarea(formulario.tarea.value)   
+function borrarTarea(identificador) {
+  let boton = document.getElementById(`${identificador}`);
+  if (boton !== null) {
+    let li = boton.parentElement;
+    li.parentElement.removeChild(li);
+  }
 }
 
-function agregarTarea(tarea){
-    crearTarea(tarea)
-    let elementoLista = document.createElement('li')
-    elementoLista.innerHTML=`${tarea}<button class="ms-3 btn  btn-borrar" id="btn-${identificador}"><i class="bi bi-x-square-fill"></i></button>`
-    // botonesEliminar.appendChild()
-    identificador ++
-}
-
-let botonesEliminar = document.querySelectorAll('.btn-borrar')
-botonesEliminar.forEach(boton => {
-    boton.addEventListener('click', () => {
-        borrarTarea(boton.id)
-    })
-})
-
-function borrarTarea(identificador){
-    let boton = document.getElementById(`${identificador}`)
-    let li = boton.parentElement
-    li.parentElement.removeChild(li)
-}
-
-
-formulario.addEventListener('submit',capturarDatos)
+formulario.addEventListener("submit", capturarDatos);
